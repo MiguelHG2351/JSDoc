62b24485a15e3f79925e02b88eb429a51b056a5f
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2020-12-16 18:01:55 -0600

    workspaces vscode, typescript with ts-check

---
+++ src/arrays.js
@@ -0,0 +1,5 @@
+/**
+ * Edades
+ * @type {number}
+ */
+let age = [25, 21, 12]
---
+++ src/index.js
@@ -0,0 +1,8 @@
+//@ts-check
+
+/**
+ * This is fullname of user
+ * @type {string}
+ */
+
+let xd = "Miguel Angel Hernández Gaitan";
